c38a5a7ce57b65d46b6f00c6b8baddd5faffdcae
Your Name <you@example.com>
2023-10-30 07:50:29 -0500

    random ducks

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let randomPoints2 = [];
 let numFlashPoints = 5;
 let numNormalPoints = 10;
 let spacing;
-let timer = 3;
+let timer = 30;
 let score = 0;
 let timerInterval;
 let isTimeUp = false;
@@ -27,7 +27,7 @@ function preload() {
 function setup() {
   createCanvas(700, 600);
   spacing = width / (numNormalPoints + 1);
-  
+
   for (let i = 0; i < numFlashPoints; i++) {
     randomPoints1.push({
       position: createVector(random(0, width), random(0, height)),
@@ -35,7 +35,7 @@ function setup() {
       isFlash: true
     });
   }
-    
+
   for (let i = 0; i < numNormalPoints; i++) {
     randomPoints2.push({
       position: createVector(random(0, width), random(0, height)),
@@ -43,36 +43,38 @@ function setup() {
       isFlash: false
     });
   }
-  
+
   timerInterval = setInterval(decrementTimer, 1000);
 }
 
 function draw() {
   background(220);
-  
+
   if (isTimeUp) {
     noLoop();
     clearInterval(timerInterval);
     textSize(32);
     fill(0);
-    text("¡Time's up!", width / 2.8, height / 2.2);
-    text("Final Score: " + score, width / 3, height / 1.8)
+    text("¡Time's up!" , width / 2.8, height / 2.2);
+    text("Final Score: "+ score,  width / 3, height / 1.8)
     return;
   }
-  
+
   updateTimerDisplay();
 
   for (let i = 0; i < numFlashPoints; i++) {
     randomPoints1[i].position.x += 13;
     if (randomPoints1[i].position.x > width) {
-      randomPoints1[i].position.x = 0;
+      randomPoints1[i].position.x = -60; // Mueve el objeto fuera de la pantalla
+      randomPoints1[i].position.y = random(0, height); // Ubicación aleatoria en el eje Y
     }
   }
 
   for (let i = 0; i < numNormalPoints; i++) {
     randomPoints2[i].position.x += 6;
     if (randomPoints2[i].position.x > width) {
-      randomPoints2[i].position.x = 0;
+      randomPoints2[i].position.x = -60; // Mueve el objeto fuera de la pantalla
+      randomPoints2[i].position.y = random(0, height); // Ubicación aleatoria en el eje Y
     }
   }
 
@@ -81,8 +83,9 @@ function draw() {
     if (mouseIsPressed) {
       const d = dist(mouseX, mouseY, randomPoints1[i].position.x + 30, randomPoints1[i].position.y + 30);
       if (d < 30) {
-        score += 3;
-        randomPoints1[i].position.x = -100;
+        score += 1;
+        randomPoints1[i].position.x = -60;
+        randomPoints1[i].position.y = random(0, height);
       }
     }
   }
@@ -93,7 +96,8 @@ function draw() {
       const d = dist(mouseX, mouseY, randomPoints2[i].position.x + 30, randomPoints2[i].position.y + 30);
       if (d < 30) {
         score += 1;
-        randomPoints2[i].position.x = -100;
+        randomPoints2[i].position.x = -60;
+        randomPoints2[i].position.y = random(0, height);
       }
     }
   }
